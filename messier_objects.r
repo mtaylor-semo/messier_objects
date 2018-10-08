559e89e73635a41907981e4a101706f268b0d1e2
Michael S. Taylor (home) <mtaylor@semo.edu>
2018-10-08 07:35:08 -0500

    Separate data in some columns.

--- messier_objects.r
+++ messier_objects.r
@@ -13,11 +13,21 @@ mess_table <-
   html_table(fill = TRUE) %>%
   .[[1]]
 
-## Remove unneeded columns
+## Remove unneeded columns and
+## delete last row that repeats column headers
 mess_table <- 
   mess_table %>% 
-  select(-c(Picture, `Right Ascension`, Declination))
-
-## Delete last row, which repeats column headers
-mess_table <- mess_table %>% 
+  select(-c(Picture, `Right Ascension`, Declination)) %>% 
   filter(!`Messier number` == "Messier number")
+
+
+## Split the Object type into two categories for
+## sorting by object rather than Messier number, then
+## Remove the Wiki footnotes in brackets after
+## the Messier numbers in the first column
+
+mess_table <- mess_table %>% 
+  separate(`Object type`,
+           into = c('object_type','subtype'), sep = ",") %>% 
+  separate(`Messier number`, into = "mess_num", sep = "\\[", extra = "drop")
+
